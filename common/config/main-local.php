4dec7bc453a59a95f9e764516c76f3328df8da88
Hafiz Baharudin <hafiz.baharudin@petronas.com.my>
2019-05-29 23:24:25 +0800

    Initial commit

---
+++ common/config/main-local.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    'components' => [
+        // 'db' => [
+        //     'class' => 'yii\db\Connection',
+        //     'dsn' => 'mysql:host=localhost:3306;dbname=igo',
+        //     'username' => 'root',
+        //     'password' => '06koi5403',
+        //     'charset' => 'utf8',
+        // ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=mybuildi_igo',
+            'username' => 'mybuildi_igo',
+            'password' => 'OAxmCr%j[B=L',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+            ],
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+        ],
+    ],
+];
